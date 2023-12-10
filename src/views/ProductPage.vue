de72f9ba6c97d37153dacc0cb54af395a94657e5
mohtzz94@gmail.com <admin@Rachels-MacBook.local>
2023-12-10 13:28:34 +0630

    added product page

---
+++ src/views/ProductPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>Product Page</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Product Page"
+    }
+</script>
+
+<style scoped>
+
+</style>
